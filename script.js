a78273964ff6f490563737d27c1fa6be9eea7db2
PaWo0508 <50174887+PaWo0508@users.noreply.github.com>
2020-09-16 00:12:29 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -11,3 +11,17 @@ $('.off').on('click', evt => {
   $form[0].reset()
   return false
 })
+
+const $form = $('#form3')
+$('.on').on('click', evt => {
+  $form.submit()
+  $form[0].reset()
+  return false
+})
+
+const $form = $('#form4')
+$('.off').on('click', evt => {
+  $form.submit()
+  $form[0].reset()
+  return false
+})
